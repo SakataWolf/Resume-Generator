e561275f322cb575996f65ed7421f7a18057333d
Alison Bruno <87884401+SakataWolf@users.noreply.github.com>
2022-03-22 21:03:19 -0300

    Add files via upload

---
+++ curriculo.php
@@ -0,0 +1,59 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>Curriculo</title>
+	<style type="text/css">
+		@import url('https://fonts.googleapis.com/css2?family=Coda&family=Cormorant+SC&family=Raleway&display=swap');
+		@import url('https://fonts.googleapis.com/css2?family=Coda&family=Cormorant+SC&family=Lora&family=Raleway&display=swap');
+		.retangulo{
+			display: block;
+			margin-left: auto;
+			margin-right: auto;
+			position: relative;
+			width: 291px;
+			height: 24px;
+			background: rgba(161, 227, 172, 0.42);
+		}
+		.nome{
+			position: absolute;
+			top: 0%;
+			/*left:50%;*/
+			font-family: 'Raleway';
+			font-style: normal;
+			font-weight: 400;
+			font-size: 24px;
+			line-height: 128.9%;
+			display: flex;
+			justify-content: center;
+			text-align: center;
+			letter-spacing: 0.365em;
+			text-indent: 1px;
+
+			color: #000000;
+		}
+	</style>
+</head>
+<body>
+	
+		
+	
+</body>
+</html>
+<?php
+/*if ($_POST) {
+	$nome = $_POST['nome']
+	$idade = $_POST['idade']
+	$endereco = $_POST['endereco']
+	$escolaridade = $_POST['escolaridade']
+	$objetivo = $_POST['objetivo']
+	$experiencia = $_POST['experiencia']
+	$cursos = isset($_POST['cursos'])?
+	$habilidade = isset($_POST['habilidade'])?
+	$email = isset($_POST['email'])?
+	$telefone = $_POST['telefone']
+	$site = isset($_POST['site'])?*/
+	echo "<div class=\"retangulo\"><h1 class = 'nome'>Alison Bruno </h1></div>";
+/*}*/
+?>
